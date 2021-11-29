980ea349dafefbdd62111bd7d65ee1fc4f308fc3
je-l <je-l@users.noreply.github.com>
2021-11-29 14:53:36 +0200

    Add __all__ special variable to __init__.py
    __all__ is required so that tools like mypy will treat the imports as
    public.

--- fmi/__init__.py
+++ fmi/__init__.py
@@ -1,3 +1,13 @@
 from .model import Observation, Forecast, Coordinates
 from .fmi import latest_observations, forecast, weather_now, sea_levels
 from .__version__ import __version__
+
+__all__ = [
+    "Observation",
+    "Forecast",
+    "Coordinates",
+    "latest_observations",
+    "forecast",
+    "weather_now",
+    "sea_levels"
+]
